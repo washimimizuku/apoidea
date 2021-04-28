6ea943245bb57b7f06350b014ee7605d00898fff
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-04-28 11:49:57 +0100

    Remove comments

--- apoidea/database.php
+++ apoidea/database.php
@@ -3,14 +3,8 @@
    $server = "localhost";
    $user = "webuser";
    $password = "webuser";
-	#$user = "irmaosme";
-	#$password = "oe1988";
-	#$user = "simplice";
-	#$password = "theman";
 	
    
    $con = mysql_pconnect($server, $user, $password) or die("Could not connect: " . mysql_error());
-   #mysql_select_db("irmaosme_apoidea2");
    mysql_select_db("apoidea");
-   #mysql_select_db("simplice_apoidea");
 ?>
